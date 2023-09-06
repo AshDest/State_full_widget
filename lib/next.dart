9460ea576cee777c8cf64a50d9e212ce1884551a
AshDest <ashuzamaheshe4@gmail.com>
2023-09-06 16:38:49 +0200

    Ash commit

--- lib/next.dart
+++ lib/next.dart
@@ -8,15 +8,33 @@ class Next extends StatefulWidget{
 }
 
 class NextState extends State<Next>{
+  bool showApp = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: (showApp)? AppBar(
         title: const Text('New Page'),
         backgroundColor: widget.color,
-      ),
+      ): null,
       body: Center(
-        child: const Text('That is my Next Page'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: (){
+                setState(() {
+                  showApp = !showApp;
+                });
+              },
+              child: const Text('Toggle App Bar'),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: const Text('Retour'),
+            ),
+          ],
+        )
       ),
     );
   }
